5a9a6229eed0292f4754eac518a7d7dca19337fb
Mallika Mondal <malmonda@in.ibm.com>
2021-07-01 22:03:11 +0530

    updated java

--- Java/Activities/Hello.java
+++ Java/Activities/Hello.java
@@ -3,7 +3,7 @@ package com.sdet.JavaActivity1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("<<HELLO MOUNIKA.. WELCOME TO JAVA WORLD>>");
+		System.out.println("<<HELLO .. WELCOME TO JAVA WORLD>>");
 	}
 
 }
